3f81b6f3ca5dfe18166ed5525fa5004dd5c714f8
charlesczar <charlesczarlucas@gmail.com>
2026-09-26 14:12:17 +0800

    edit

--- stage1/config.py
+++ stage1/config.py
@@ -6,7 +6,6 @@ DATA_DIR = PROJECT_ROOT / "data"
 MODEL_DIR = PROJECT_ROOT / "models" / "dost_roberta"
 DATA_PATH = DATA_DIR / "test_reviews.csv"
 FEATURES_PATH = DATA_DIR / "6d_features.csv"
-STAGE1_OUTPUT_DIR = PROJECT_ROOT / "stage1" / "temp_results"
 BASE_MODEL = "dost-asti/RoBERTa-tl-cased"
 
 # --- Data Processing ---
